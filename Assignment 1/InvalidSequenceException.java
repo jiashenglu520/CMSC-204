4073227a28da7a1a695a1f635049de18d0bc856f
jiashenglu520 <60863705+jiashenglu520@users.noreply.github.com>
2020-09-16 22:30:17 -0400

    Add files via upload

---
+++ Assignment 1/InvalidSequenceException.java
@@ -0,0 +1,13 @@
+public class InvalidSequenceException extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8637208455584592176L;
+
+	//Parameterized constructor
+	public InvalidSequenceException (String message) {
+		super(message);
+	}
+
+}
---
+++ Assignment 1/NoLowerAlphaException.java
@@ -0,0 +1,13 @@
+public class NoLowerAlphaException extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8740404453434962706L;
+
+	//Parameterized constructor
+	public NoLowerAlphaException (String message) {
+		super(message);
+	}
+
+}
